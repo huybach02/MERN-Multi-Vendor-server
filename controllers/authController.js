ef03d7ef2bb1a99c005d271a18b064a4b68c6fec
huy bach <lhbach2002ct@gmail.com>
2023-12-16 12:41:01 +0700

    v1

---
+++ controllers/authController.js
@@ -0,0 +1,9 @@
+const admin_login = async (req, res) => {
+  console.log(req.body);
+};
+
+const authController = {
+  admin_login,
+};
+
+module.exports = authController;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use("/api", require("./routes/authRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
